58afa36b1c5eb10e84e1c7938ffb366c6dcb0a3f
vincentfree <neromy_1@msn.com>
2015-12-21 14:34:27 +0100

    Bootstrap Spring Boot

---
+++ src/main/nl/atos/ontwikkelstraat/OntwikkelstraatApplication.java
@@ -0,0 +1,12 @@
+package nl.atos.ontwikkelstraat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntwikkelstraatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OntwikkelstraatApplication.class, args);
+    }
+}
