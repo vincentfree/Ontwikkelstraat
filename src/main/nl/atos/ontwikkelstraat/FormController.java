e238080cc2cde41ce3232ba58ffeef971289eb75
jbilkes@hotmail.com <jbilkes@hotmail.com>
2015-12-23 14:19:43 +0000

    added data processing

--- src/main/nl/atos/ontwikkelstraat/FormController.java
+++ src/main/nl/atos/ontwikkelstraat/FormController.java
@@ -2,8 +2,11 @@ package nl.atos.ontwikkelstraat;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pojo.FormData;
 
 /**
  * Created by nerom on 23-12-2015.
@@ -11,13 +14,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
+@RequestMapping("/form")
 public class FormController {
     /**
      * @return hello world!
      */
-    @RequestMapping("/form")
+    @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
-    String home() {
-        return "Hello World!";
+    void processData(@RequestBody FormData data) {
+
+//        data.name;
+//        data.surname;
+//        data.address;
+//        data.houseNumber;
+//        data.postalCode;
+
+        System.out.println(data.getName());
+        System.out.println(data.getSurname());
+        System.out.println(data.getHouseNumber());
+        System.out.println(data.getPostalCode());
+        System.out.println(data.getAddress());
+
     }
 }
---
+++ src/main/pojo/FormData.java
@@ -0,0 +1,53 @@
+package pojo;
+
+/**
+ * Created by vagrant on 12/23/15.
+ */
+public class FormData {
+
+    private String name;
+    private String surname;
+    private String address;
+    private int houseNumber;
+    private String postalCode;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+}
