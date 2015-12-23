add2565ec41813577b3f802ff50b8a4d63c7d7f6
jbilkes@hotmail.com <jbilkes@hotmail.com>
2015-12-23 15:03:13 +0000

    fixed backend

--- src/main/nl/atos/ontwikkelstraat/FormController.java
+++ src/main/nl/atos/ontwikkelstraat/FormController.java
@@ -1,39 +1,33 @@
 package nl.atos.ontwikkelstraat;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import pojo.FormData;
 
-/**
- * Created by nerom on 23-12-2015.
- */
 
 @Controller
 @EnableAutoConfiguration
 @RequestMapping("/form")
 public class FormController {
-    /**
-     * @return hello world!
-     */
+
     @RequestMapping(method = RequestMethod.POST)
-    @ResponseBody
-    void processData(@RequestBody FormData data) {
+    @ResponseStatus (HttpStatus.CREATED)
+    public void processData(
+            @RequestParam() String name,
+            @RequestParam() String surName,
+            @RequestParam() String address,
+            @RequestParam() String houseNumber,
+            @RequestParam() String zipCode) {
 
-//        data.name;
-//        data.surname;
-//        data.address;
-//        data.houseNumber;
-//        data.postalCode;
-
-        System.out.println(data.getName());
-        System.out.println(data.getSurname());
-        System.out.println(data.getHouseNumber());
-        System.out.println(data.getPostalCode());
-        System.out.println(data.getAddress());
-
+        System.out.println("name: " + name);
+        System.out.println("surName: " + surName);
+        System.out.println("zipCode: " + zipCode);
+        System.out.println("houseNumber: " + houseNumber);
+        System.out.println("address: " + address);
     }
 }
