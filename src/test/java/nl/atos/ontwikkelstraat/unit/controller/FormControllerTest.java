23dea393cbbe4fa955cb60e4321ec56783d49801
jbilkes@hotmail.com <jbilkes@hotmail.com>
2015-12-29 09:42:04 +0000

    added more controller unit tests

--- src/test/java/nl/atos/ontwikkelstraat/unit/controller/FormControllerTest.java
+++ src/test/java/nl/atos/ontwikkelstraat/unit/controller/FormControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
@@ -23,6 +23,24 @@ public class FormControllerTest {
     @Test
     public void index() throws Exception {
         this.mockMvc.perform(get("/").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void addNewPost() throws Exception {
+        this.mockMvc.perform(post("/").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isOk());
+    }
+
+    @Test (expected = AssertionError.class)
+    public void addNewGetWrongEndpoint() throws Exception {
+        this.mockMvc.perform(get("/ffefew").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isOk());
+    }
+
+    @Test (expected = AssertionError.class)
+    public void addNewPostWrongEndpoint() throws Exception {
+        this.mockMvc.perform(post("/ffefew").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
                 .andExpect(status().isOk());
     }
 
