a05a9dfe01485493a87d55b36b7a0657df9a0f2d
jbilkes@hotmail.com <jbilkes@hotmail.com>
2015-12-29 09:27:16 +0000

    added controller unit testt

--- src/main/java/nl/atos/ontwikkelstraat/controller/FormController.java
+++ src/main/java/nl/atos/ontwikkelstraat/controller/FormController.java
@@ -1,12 +1,14 @@
 package nl.atos.ontwikkelstraat.controller;
 
 import nl.atos.ontwikkelstraat.pojo.FormData;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.validation.Valid;
 
@@ -14,6 +16,7 @@ import javax.validation.Valid;
 public class FormController {
 
     @RequestMapping(value="/", method=RequestMethod.GET)
+    @ResponseStatus (HttpStatus.NO_CONTENT)
     public String index(FormData formData) {
         return "index";
     }
--- src/test/java/nl/atos/ontwikkelstraat/unit/controller/FormControllerTest.java
+++ src/test/java/nl/atos/ontwikkelstraat/unit/controller/FormControllerTest.java
@@ -1,14 +1,29 @@
 package nl.atos.ontwikkelstraat.unit.controller;
 
+import nl.atos.ontwikkelstraat.controller.FormController;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 public class FormControllerTest {
 
-    @Test
-    public void index(){
-        //blabla
+    private MockMvc mockMvc;
+
+
+    @Before
+    public void setup() {
+        this.mockMvc = standaloneSetup(new FormController()).build();
     }
 
+    @Test
+    public void index() throws Exception {
+        this.mockMvc.perform(get("/").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isOk());
+    }
 
 }
