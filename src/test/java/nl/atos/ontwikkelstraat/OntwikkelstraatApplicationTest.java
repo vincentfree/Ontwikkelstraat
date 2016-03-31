30790f64d552883c65cec115786fba26019fb49f
vincentfree <neromy_1@msn.com>
2016-03-31 16:22:17 +0200

    versie naar 1.0.0 em imports geupdate

---
+++ src/test/java/nl/atos/ontwikkelstraat/OntwikkelstraatApplicationTest.java
@@ -0,0 +1,26 @@
+package nl.atos.ontwikkelstraat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by nerom on 31-3-2016.
+ */
+public class OntwikkelstraatApplicationTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDwon() {
+
+    }
+
+    @Test
+    public void mainApplication() {
+        OntwikkelstraatApplication.main(new String[]{});
+    }
+}
