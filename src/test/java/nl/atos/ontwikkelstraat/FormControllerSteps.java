6602231948634942af4ce9d1529c9235d8245aa8
vincentfree <neromy_1@msn.com>
2016-04-02 18:02:11 +0200

    low cap testcases for cucumber

--- src/test/java/nl/atos/ontwikkelstraat/FormControllerSteps.java
+++ src/test/java/nl/atos/ontwikkelstraat/FormControllerSteps.java
@@ -35,7 +35,7 @@ public class FormControllerSteps {
     }
 
     @Given("^That the website is running and I navigated to it$")
-    public void That_the_website_is_running_and_I_navigated_to_it() throws InterruptedException {
+    public void that_the_website_is_running_and_I_navigated_to_it() throws InterruptedException {
         webdriver.manage().window().maximize();
         webdriver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
         if (System.getenv("TRAVIS") == "true") {
@@ -45,7 +45,7 @@ public class FormControllerSteps {
     }
 
     @When("^I fill in my firstName with \"([^\"]*)\"$")
-    public void I_fill_in_my_firstName_with(String firstname) {
+    public void i_fill_in_my_firstName_with(String firstname) {
         formData.setName(firstname);
         webdriver.findElement(By.id("name")).sendKeys(firstname);
         assertEquals(firstname, webdriver.findElement(By.id("name")).getAttribute("value"));
@@ -54,42 +54,42 @@ public class FormControllerSteps {
 
 
     @And("^I fill in my lastName$")
-    public void I_fill_in_my_lastName() throws Throwable {
+    public void i_fill_in_my_lastName() throws Throwable {
         formData.setSurname("Johnson");
         webdriver.findElement(By.id("surname")).sendKeys("Johnson");
         assertEquals("Johnson", webdriver.findElement(By.id("surname")).getAttribute("value"));
     }
 
     @And("^I fill in my StreetName$")
-    public void I_fill_in_my_StreetName() throws Throwable {
+    public void i_fill_in_my_StreetName() throws Throwable {
         formData.setStreetName("hoofdstraat");
         webdriver.findElement(By.id("streetName")).sendKeys("hoofdstraat");
         assertEquals("hoofdstraat", webdriver.findElement(By.id("streetName")).getAttribute("value"));
     }
 
     @And("^I fill in my HouseNumber$")
-    public void I_fill_in_my_HouseNumber() throws Throwable {
+    public void i_fill_in_my_HouseNumber() throws Throwable {
         formData.setHouseNumber(134);
         webdriver.findElement(By.id("houseNumber")).sendKeys("134");
         assertEquals("134", webdriver.findElement(By.id("houseNumber")).getAttribute("value"));
     }
 
     @And("^I fill in my Zipcode$")
-    public void I_fill_in_my_Zipcode() throws Throwable {
+    public void i_fill_in_my_Zipcode() throws Throwable {
         formData.setZipCode("9485SL");
         webdriver.findElement(By.id("zipCode")).sendKeys("9485SL");
         assertEquals("9485SL", webdriver.findElement(By.id("zipCode")).getAttribute("value"));
     }
 
     @And("^I hit the submit button$")
-    public void I_hit_the_submit_button() throws Throwable {
+    public void i_hit_the_submit_button() throws Throwable {
         JavascriptExecutor jse = (JavascriptExecutor) webdriver;
         jse.executeScript("window.scrollBy(0,250)", "");
         webdriver.findElement(By.id("testSubmit")).click();
     }
 
     @Then("^I should see my name and address data on the next page and it is also saved$")
-    public void I_should_see_my_name_and_address_data_on_the_next_page_and_it_is_also_saved() throws Throwable {
+    public void i_should_see_my_name_and_address_data_on_the_next_page_and_it_is_also_saved() throws Throwable {
         assertEquals(formData.getName(), webdriver.findElement(By.id("name")).getAttribute("value"));
         assertEquals(formData.getSurname(), webdriver.findElement(By.id("surname")).getAttribute("value"));
         assertEquals(formData.getStreetName(), webdriver.findElement(By.id("streetName")).getAttribute("value"));
@@ -107,28 +107,28 @@ public class FormControllerSteps {
     }
 
     @And("^I fill in my lastName with \"([^\"]*)\"$")
-    public void I_fill_in_my_lastName_with(String lastName) throws Throwable {
+    public void i_fill_in_my_lastName_with(String lastName) throws Throwable {
         formData.setSurname(lastName);
         webdriver.findElement(By.id("surname")).sendKeys(lastName);
         assertEquals(lastName, webdriver.findElement(By.id("surname")).getAttribute("value"));
     }
 
     @And("^I fill in my StreetName with \"([^\"]*)\"$")
-    public void I_fill_in_my_StreetName_with(String streetName) throws Throwable {
+    public void i_fill_in_my_StreetName_with(String streetName) throws Throwable {
         formData.setStreetName(streetName);
         webdriver.findElement(By.id("streetName")).sendKeys(streetName);
         assertEquals(streetName, webdriver.findElement(By.id("streetName")).getAttribute("value"));
     }
 
     @And("^I fill in my HouseNumber with \"([^\"]*)\"$")
-    public void I_fill_in_my_HouseNumber_with(String houseNumber) throws Throwable {
+    public void i_fill_in_my_HouseNumber_with(String houseNumber) throws Throwable {
         formData.setHouseNumber(Integer.parseInt(houseNumber));
         webdriver.findElement(By.id("houseNumber")).sendKeys(houseNumber);
         assertEquals(houseNumber, webdriver.findElement(By.id("houseNumber")).getAttribute("value"));
     }
 
     @And("^I fill in my Zipcode with \"([^\"]*)\"$")
-    public void I_fill_in_my_Zipcode_with(String zipCode) throws Throwable {
+    public void i_fill_in_my_Zipcode_with(String zipCode) throws Throwable {
         formData.setZipCode(zipCode);
         webdriver.findElement(By.id("zipCode")).sendKeys(zipCode);
         assertEquals(zipCode, webdriver.findElement(By.id("zipCode")).getAttribute("value"));
